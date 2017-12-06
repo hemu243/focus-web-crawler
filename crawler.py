e673f21478b92ead9a149cecdef9d9b045248895
Hemendra Choudhary <hchoudhary@splunk.com>
2017-12-05 22:49:42 -0800

    temp logging code

--- crawler.py
+++ crawler.py
@@ -7,14 +7,45 @@ import logging
 Module which start crawling using seeds urls and classifier
 """
 
-from scrapy.utils.log import configure_logging
+'''from scrapy.utils.log import configure_logging
 
 configure_logging(install_root_handler=False)
 logging.basicConfig(
-    filename='focused_crawler.log',
+    filename='log/focused_crawler.log',
     format='%(levelname)s: %(message)s',
     level=logging.INFO
-)
+)'''
+
+'''
+def setup_logging(log_name, logger_name, logger=None, level=logging.INFO, is_console_header=False,
+                  log_format='%(asctime)s %(levelname)s [%(name)s] [%(module)s] [%(funcName)s] [%(process)d]'
+                             ' %(message)s', is_propagate=False):
+
+    if log_name is None or logger_name is None:
+        raise ValueError("log_name or logger_name is not specified")
+
+    if logger is None:
+        # Logger is singleton so if logger is already defined it will return old handler
+        logger = logging.getLogger(logger_name)
+
+    logger.propagate = is_propagate  # Prevent the log messages from being duplicated in the python.log file
+    logger.setLevel(level)
+
+    file_handler = logging.handlers.RotatingFileHandler(make_splunkhome_path(['var', 'log', 'splunk', log_name]),
+                                                            maxBytes=2500000, backupCount=5)
+        formatter = logging.Formatter(log_format)
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+
+        # Console stream handler
+        if is_console_header:
+            console_handler = logging.StreamHandler()
+            console_handler.setFormatter(logging.Formatter(log_format))
+            logger.addHandler(console_handler)
+            
+    return logger
+'''
+
 
 def starCrawl(input_urls="http://newhomesource.com", classifier=None):
 	"""
--- focused_scrapy_crawler/pipelines.py
+++ focused_scrapy_crawler/pipelines.py
@@ -4,9 +4,9 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
-import json
+
 from scrapy.exceptions import DropItem
-import time
+
 
 class RemoveDupLink(object):
     """
